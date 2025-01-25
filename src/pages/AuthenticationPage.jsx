391ba14f78cc4be0dd0140d59047d1fc79e33026
Shrey Singhal <shreynbd12@gmail.com>
2025-01-25 18:59:39 +0530

    Make authentication Page

---
+++ src/pages/AuthenticationPage.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export const loginPage = () => {
+    return (
+        <div>
+            <h1>Login Page</h1>
+        </div>
+    )
+}
+
+export const registerPage = () => {
+    return (
+        <div>
+            <h1>Register Page</h1>
+        </div>
+    )
+}
