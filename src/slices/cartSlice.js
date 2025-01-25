5da6f9928ef388a338df10a8105bb3bb5310c3d9
Shrey Singhal <shreynbd12@gmail.com>
2025-01-25 18:23:34 +0530

    Set Cart using cartSlice

--- src/slices/cartSlice.js
+++ src/slices/cartSlice.js
@@ -8,15 +8,12 @@ export const cartSlice = createSlice({
     name: 'cart',
     initialState,
     reducers: {
-        addToCart: (state, action) => {
-            state.items.push(action.payload);
-        },
-        removeFromCart: (state, action) => {
-            state.items = state.items.filter(item => item.id !== action.payload.id);
+        setCart: (state, action) => {
+            state.items = action.payload;
         },
     },
 });
 
-export const { addToCart, removeFromCart } = cartSlice.actions;
+export const { setCart } = cartSlice.actions;
 
 export default cartSlice.reducer;
