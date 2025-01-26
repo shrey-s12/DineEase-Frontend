7516f4793f8949cf597778d4ef08f205a8c7b0da
Shrey Singhal <shreynbd12@gmail.com>
2025-01-26 22:01:48 +0530

    Create thunk for addToCart

--- src/slices/cartSlice.js
+++ src/slices/cartSlice.js
@@ -1,4 +1,6 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import axios from 'axios';
+const MAIN_URL = import.meta.env.VITE_MAIN_API_URL;
 
 const initialState = {
     items: [],
@@ -12,8 +14,22 @@ export const cartSlice = createSlice({
             state.items = action.payload;
         },
     },
+    extraReducers: (builder) => {
+        builder.addCase(addToCart.fulfilled, (state, action) => {
+            state.items = action.payload;
+        });
+        builder.addCase(addToCart.rejected, (state, action) => {
+            console.log(action);
+        });
+    }
 });
 
 export const { setCart } = cartSlice.actions;
 
 export default cartSlice.reducer;
+
+export const addToCart = createAsyncThunk('cart/addToCart', async (dishId) => {
+    const response = await axios.post(`${MAIN_URL}/cart/${dishId}`);
+    return response.data;
+});
+
