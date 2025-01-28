0645ef387e0e636ba37b37a50792b951374462a2
Shrey Singhal <shreynbd12@gmail.com>
2025-01-29 00:16:44 +0530

    Show the owners name in the counter component

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -2,10 +2,17 @@ import React from 'react'
 
 
 const Counter = ({ counter }) => {
+  console.log("Counter", counter)
   return (
     <div className="border border-gray-200 p-4 m-4">
       <h2 className="text-lg font-bold">{counter.name}</h2>
       <p>{counter.description}</p>
+      <span className='font-medium'>Owners</span>
+      {counter.merchants.map((merchant) => (
+        <div key={merchant._id} className="flex justify-between items-center">
+          <span>{merchant.name}</span>
+        </div>
+      ))}
     </div>
   )
 }
