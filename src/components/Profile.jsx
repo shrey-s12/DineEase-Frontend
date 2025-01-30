ef070f99db71bc9ac57ac76c88d974c145f90f19
Shrey Singhal <shreynbd12@gmail.com>
2025-01-30 14:14:07 +0530

    Correct the error that comes when i load the ProfilePage

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -3,9 +3,9 @@ import React from 'react'
 const Profile = ({ user }) => {
     return (
         <div>
-            <p>Name: {user.name}</p>
-            <p>Email: {user.email}</p>
-            <p>Role: {user.role}</p>
+            <p>Name: {user?.name}</p>
+            <p>Email: {user?.email}</p>
+            <p>Role: {user?.role}</p>
         </div>
     )
 }
