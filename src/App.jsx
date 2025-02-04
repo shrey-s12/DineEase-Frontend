4b36fe6ffe54afd0166b2c9eed0e01521520dbda
Shrey Singhal <shreynbd12@gmail.com>
2025-02-04 18:14:26 +0530

    Auth Route and Loading

--- src/App.jsx
+++ src/App.jsx
@@ -8,9 +8,9 @@ import CountersPage from "./pages/CountersPage"
 import DishesPage from "./pages/DishesPage"
 import ProfilePage from "./pages/ProfilePage"
 import { useDispatch, useSelector } from "react-redux"
-import { setUser } from "./slices/authSlice"
+import { setUser, setLoading } from "./slices/authSlice.js"
 import { setCart } from "./slices/cartSlice"
-import { LoginPage, RegisterPage } from "./pages/AuthenticationPage"
+import { Auth, LoginPage, RegisterPage } from "./pages/AuthenticationPage"
 import DishesByCounter from "./components/DishesByCounter"
 import AllUsersPage from "./pages/AllUsersPage"
 import CreateCounterPage from "./pages/CreateCounterPage"
@@ -21,54 +21,74 @@ const MAIN_URL = import.meta.env.VITE_MAIN_API_URL;
 function App() {
   const dispatch = useDispatch();
   const user = useSelector(state => state.auth.user);
+  const loading = useSelector(state => state.auth.loading);
+  const token = localStorage.getItem("token");
+
+  const fetchUser = async (token) => {
+    dispatch(setLoading(true));
+    try {
+      const res = await axios.get(`${MAIN_URL}/cart`, {
+        headers: { Authorization: `Bearer ${token}` }
+      })
+      const user = res.data;
+      dispatch(setUser(user));
+    } catch (err) {
+      console.error(err.message);
+    } finally {
+      dispatch(setLoading(false));
+    }
+  }
+
+  const fetchCart = async (token) => {
+    try {
+      const res = await axios.get(`${MAIN_URL}/cart`, {
+        headers: { Authorization: `Bearer ${token}` }
+      })
+      const cart = res.data.cart;
+      dispatch(setCart(cart));
+    } catch (err) {
+      console.error(err.message);
+    }
+  }
 
   useEffect(() => {
-    async function userInfo(token) {
-      try {
-        const res = await axios.get(`${MAIN_URL}/cart`, {
-          headers: { Authorization: `Bearer ${token}` }
-        })
-        const cart = res.data.cart;
-        dispatch(setCart(cart));
-      } catch (err) {
-        console.error(err.message);
-      }
+    if (!user) {
+      fetchUser(token);
     }
-    userInfo(localStorage.getItem("token"))
-  }, [user])
+  }, [user]);
 
   useEffect(() => {
-    async function userInfo(token) {
-      try {
-        const res = await axios.get(`${MAIN_URL}/cart`, {
-          headers: { Authorization: `Bearer ${token}` }
-        })
-        const user = res.data;
-        dispatch(setUser(user));
-      } catch (err) {
-        console.error(err.message);
-      }
-    }
-    userInfo(localStorage.getItem("token"))
-  }, [])
+    fetchCart(token);
+  }, []);
+
+  if (loading) {
+    return (
+      <div className="flex justify-center items-center h-screen">
+        <div className="animate-spin rounded-full h-32 w-32 border-t-2 border-b-2 border-gray-900"></div>
+      </div>
+    )
+  }
 
   return (
     <div>
-      <Navbar />
       <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/cart" element={<CartPage />} />
-        <Route path="/counters" element={<CountersPage />} />
-        <Route path="/dishes" element={<DishesPage />} />
-        <Route path="/profile" element={<ProfilePage />} />
+        <Route element={<Navbar />}>
+          <Route element={<Auth />}>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/cart" element={<CartPage />} />
+            <Route path="/counters" element={<CountersPage />} />
+            <Route path="/dishes" element={<DishesPage />} />
+            <Route path="/profile" element={<ProfilePage />} />
+            <Route path="/dish/counter/:counterId" element={<DishesByCounter />} />
+            <Route path="/allUsers" element={<AllUsersPage />} />
+            <Route path="/dish/counter/create" element={<CreateCounterPage />} />
+            <Route path="/dish/create" element={<CreateDishPage />} />
+          </Route>
+        </Route>
         <Route path="/auth/login" element={<LoginPage />} />
         <Route path="/auth/register" element={<RegisterPage />} />
-        <Route path="/dish/counter/:counterId" element={<DishesByCounter />} />
-        <Route path="/allUsers" element={<AllUsersPage />} />
-        <Route path="/dish/counter/create" element={<CreateCounterPage />} />
-        <Route path="/dish/create" element={<CreateDishPage />} />
       </Routes>
-      <Footer />
+      {user && <Footer />}
     </div>
   )
 }
