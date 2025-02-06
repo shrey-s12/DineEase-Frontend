ecbd8a98b4a7de22f77524ad10b04a3577ec68d0
Shrey Singhal <shreynbd12@gmail.com>
2025-02-06 17:40:22 +0530

    Show the user name in the navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -16,67 +16,68 @@ const Navbar = () => {
     return (
         <>
             <nav className="flex justify-between items-center bg-gray-900 px-6 py-3 shadow-md sticky top-0 z-50">
-                {/* Logo */}
-                <Link to="/" className="text-white font-extrabold text-3xl hover:text-yellow-400">
-                    DineEase
-                </Link>
 
-                {/* Links */}
-                <div className="flex space-x-6">
-                    <Link
-                        to="/allUsers"
-                        className="text-white text-lg font-medium hover:text-yellow-400 transition"
-                    >
-                        All Users
-                    </Link>
-                    <Link
-                        to="/counters"
-                        className="text-white text-lg font-medium hover:text-yellow-400 transition"
-                    >
-                        Counters
-                    </Link>
-                    <Link
-                        to="/dishes"
-                        className="text-white text-lg font-medium hover:text-yellow-400 transition"
-                    >
-                        Dishes
-                    </Link>
-                    <Link
-                        to="/profile"
-                        className="text-white text-lg font-medium hover:text-yellow-400 transition"
-                    >
-                        Profile
-                    </Link>
-                    {user
-                        ? <Link
-                            onClick={handleLogout}
-                            to="/auth/login"
-                            className="text-white text-lg font-medium hover:text-yellow-400 transition"
-                        >
-                            Logout
+                {/* if no user */}
+                {!user && (
+                    <>
+                        < Link to="/" className="text-white font-extrabold text-3xl hover:text-yellow-400">
+                            DineEase
                         </Link>
-                        : <Link
-                            to="/auth/login"
-                            className="text-white text-lg font-medium hover:text-yellow-400 transition"
-                        >
-                            Login
+                        <div className="flex space-x-6">
+                            <Link to="/auth/login" className="text-white text-lg font-medium hover:text-yellow-400 transition" >
+                                Login
+                            </Link>
+                        </div>
+                    </>
+                )}
+
+                {/* if user */}
+                {user && (
+                    <>
+                        <Link to="/" className="text-white font-extrabold text-3xl hover:text-yellow-400">
+                            <span>DineEase</span>
+                            <span className='text-sm font-semibold ml-2'>{user.name}</span>
                         </Link>
-                    }
-                    {/* Cart */}
-                    <Link
-                        to="/cart"
-                        className="relative flex items-center text-white font-medium hover:text-yellow-400 transition"
-                    >
-                        <button className="relative">
-                            <LiaShoppingCartSolid className="text-3xl" />
-                            <div className="absolute -top-1 -right-2 bg-yellow-500 text-xs text-white font-bold rounded-full w-5 h-5 flex items-center justify-center">
-                                {cartCount}
-                            </div>
-                        </button>
-                        <span className="hidden md:inline ml-2">Cart</span>
-                    </Link>
-                </div>
-            </nav>
+
+                        <div className="flex space-x-6">
+                            {user.role === "Admin" && (<Link to="/allUsers" className="text-white text-lg font-medium hover:text-yellow-400 transition" >
+                                All Users
+                            </Link>
+                            )}
+
+
+
+                            <Link to="/counters" className="text-white text-lg font-medium hover:text-yellow-400 transition">
+                                Counters
+                            </Link>
+
+                            <Link to="/dishes" className="text-white text-lg font-medium hover:text-yellow-400 transition" >
+                                Dishes
+                            </Link>
+
+                            <Link to="/profile" className="text-white text-lg font-medium hover:text-yellow-400 transition" >
+                                Profile
+                            </Link>
+
+                            <Link onClick={handleLogout} to="/auth/login" className="text-white text-lg font-medium hover:text-yellow-400 transition" >
+                                Logout
+                            </Link>
+
+                            {/* Cart */}
+                            <Link to="/cart" className="relative flex items-center text-white font-medium hover:text-yellow-400 transition" >
+                                <button className="relative">
+                                    <LiaShoppingCartSolid className="text-3xl" />
+                                    <div className="absolute -top-1 -right-2 bg-yellow-500 text-xs text-white font-bold rounded-full w-5 h-5 flex items-center justify-center">
+                                        {cartCount}
+                                    </div>
+                                </button>
+                                <span className="hidden md:inline ml-2">Cart</span>
+                            </Link>
+                        </div>
+
+                    </>
+                )}
+            </nav >
             <Outlet />
         </>
 
