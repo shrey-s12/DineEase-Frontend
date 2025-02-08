b952abc02e83a142970ce4f656c21aa7fd5b338a
Shrey Singhal <shreynbd12@gmail.com>
2025-02-08 15:03:09 +0530

    Add some static data

--- src/data.js
+++ src/data.js
@@ -1,21 +1,70 @@
+const StaticCategories = [
+    // "Burger",
+    // "Pasta",
+    // "Pizza",
+    // "Shawarma",
+    // "Sushi",
+    // "Taco",
+    // "Waffle"
+    {
+        _id: "1",
+        name: "Burger",
+        image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRO_xyRaBbC4bJ2GmuSVLlUhpl0uXLuNAJ6LQ&s",
+    },
+    {
+        _id: "2",
+        name: "Pasta",
+        image: "https://img1.wsimg.com/isteam/ip/83632b42-f400-4ec0-9d34-7d32466970e2/A4C6E1A3-89B5-4F6E-AAFE-403E60AA3494.jpeg",
+    },
+    {
+        _id: "3",
+        name: "Pizza",
+        image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRZ5ws5qfvThY8ZLvzhn-O7jJsB6xmnx5W6mw&s",
+    },
+    {
+        _id: "4",
+        name: "Shawarma",
+        image: "https://media-cdn.grubhub.com/image/upload/d_search:browse-images:default.jpg/w_300,q_100,fl_lossy,dpr_2.0,c_fit,f_auto,h_300/oxpywb3ibcqd6vtszdfq",
+    },
+    {
+        _id: "5",
+        name: "Sushi",
+        image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRO_xyRaBbC4bJ2GmuSVLlUhpl0uXLuNAJ6LQ&s",
+    },
+    {
+        _id: "6",
+        name: "Taco",
+        image: "https://img1.wsimg.com/isteam/ip/83632b42-f400-4ec0-9d34-7d32466970e2/A4C6E1A3-89B5-4F6E-AAFE-403E60AA3494.jpeg",
+    },
+    {
+        _id: "7",
+        name: "Waffle",
+        image: "https://media-cdn.grubhub.com/image/upload/d_search:browse-images:default.jpg/w_300,q_100,fl_lossy,dpr_2.0,c_fit,f_auto,h_300/oxpywb3ibcqd6vtszdfq",
+    },
+]
+
 const StaticCounters = [
     {
         _id: "1",
+        image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRO_xyRaBbC4bJ2GmuSVLlUhpl0uXLuNAJ6LQ&s",
         name: "Domino's Delight",
         description: "Serving freshly baked pizzas with a variety of toppings to satisfy every craving.",
     },
     {
         _id: "2",
+        image: "https://img1.wsimg.com/isteam/ip/83632b42-f400-4ec0-9d34-7d32466970e2/A4C6E1A3-89B5-4F6E-AAFE-403E60AA3494.jpeg",
         name: "Burger Bliss",
         description: "Juicy burgers and crispy fries that hit the spot every time.",
     },
     {
         _id: "3",
+        image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRZ5ws5qfvThY8ZLvzhn-O7jJsB6xmnx5W6mw&s",
         name: "Pasta Paradise",
         description: "Handcrafted pastas with authentic sauces for a taste of Italy.",
     },
     {
         _id: "4",
+        image: "https://media-cdn.grubhub.com/image/upload/d_search:browse-images:default.jpg/w_300,q_100,fl_lossy,dpr_2.0,c_fit,f_auto,h_300/oxpywb3ibcqd6vtszdfq",
         name: "Waffle Wonder",
         description: "Sweet and savory waffles topped with your favorite flavors.",
     },
@@ -24,24 +73,28 @@ const StaticCounters = [
 const StaticDishes = [
     {
         _id: "1",
+        image: "https://www.moulinex-me.com/medias/?context=bWFzdGVyfHJvb3R8MTQzNTExfGltYWdlL2pwZWd8YUdObEwyaG1aQzh4TlRrMk9EWXlOVGM0TmpreE1DNXFjR2N8MmYwYzQ4YTg0MTgzNmVjYTZkMWZkZWZmMDdlMWFlMjRhOGIxMTQ2MTZkNDk4ZDU3ZjlkNDk2MzMzNDA5OWY3OA",
         name: "Pepperoni Pizza",
         description: "Classic pizza topped with pepperoni slices and melted cheese.",
     },
     {
         _id: "2",
-        name: "Cheeseburger",
-        description: "Juicy beef patty topped with melted cheese, lettuce, and tomato.",
+        image: "https://theeburgerdude.com/wp-content/uploads/2023/03/Double-Del-01-scaled.webp",
+        name: "Double Cheeseburger",
+        description: "Two beef patties, cheddar cheese, pickles, and special sauce.",
     },
     {
         _id: "3",
-        name: "Spaghetti Bolognese",
-        description: "Spaghetti noodles with rich meat sauce and grated parmesan cheese.",
+        image: "https://aisforappleau.com/wp-content/uploads/2023/07/how-to-make-sushi-salmon-nigiri-6.jpg",
+        name: "Salmon Sushi",
+        description: "Fresh salmon sushi rolls with a touch of wasabi and soy sauce.",
     },
     {
         _id: "4",
-        name: "Chocolate Waffle",
-        description: "Warm waffle topped with chocolate syrup and whipped cream.",
+        image: "https://content.jdmagicbox.com/v2/comp/palghar/z3/022pxx22.xx22.240919193143.p9z3/catalogue/waffle-delight-co-nalasopara-east-palghar-waffle-centres-9tnhh5kv2p.jpg",
+        name: "Waffle Delight",
+        description: "A fluffy waffle served with fresh berries, whipped cream, and syrup."
     },
 ]
 
-export { StaticCounters, StaticDishes }
+export { StaticCategories, StaticCounters, StaticDishes }
