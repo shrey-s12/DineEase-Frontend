079ca4ee31a2842f5bd4ac9b4fdc5c192ba239be
Shrey Singhal <shreynbd12@gmail.com>
2025-02-08 19:41:48 +0530

    import usersSlice in store

--- src/store.js
+++ src/store.js
@@ -2,11 +2,13 @@ import { configureStore } from '@reduxjs/toolkit';
 import cartSlice from './slices/cartSlice';
 import authSlice from './slices/authSlice';
 import counterSlice from './slices/counterSlice';
+import usersSlice from './slices/usersSlice';
 
 export default configureStore({
     reducer: {
         cart: cartSlice,
         auth: authSlice,
         counter: counterSlice,
+        users: usersSlice,
     },
 })
