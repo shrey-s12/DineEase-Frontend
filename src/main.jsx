ac9d74b5625a3471206299f08ae92069ad1559ec
Shrey Singhal <shreynbd12@gmail.com>
2025-02-08 18:26:24 +0530

    import ToastProvider in main.jsx file

--- src/main.jsx
+++ src/main.jsx
@@ -5,11 +5,13 @@ import './index.css'
 import App from './App.jsx'
 import { Provider } from 'react-redux'
 import store from './store.js'
+import ToastProvider from './components/ToastProvider.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Provider store={store}>
       <BrowserRouter >
+        <ToastProvider />
         <App />
       </BrowserRouter>
     </Provider>
