b9d05d7da85ff78255c7fc6829580dafe579a333
Shrey Singhal <shreynbd12@gmail.com>
2025-02-09 11:57:04 +0530

    Modify the loading styling

--- src/App.jsx
+++ src/App.jsx
@@ -61,8 +61,8 @@ function App() {
 
   if (loading) {
     return (
-      <div className="flex justify-center items-center h-screen">
-        <div className="animate-spin rounded-full h-32 w-32 border-t-2 border-b-2 border-gray-900"></div>
+      <div className="bg-gray-950 flex justify-center items-center h-screen">
+        <div className="animate-spin rounded-full h-32 w-32 border-t-2 border-b-2 border-gray-100"></div>
       </div>
     )
   }
