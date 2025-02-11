07778a1931937a427f0ca9825991462aa7dfb60f
Shrey Singhal <shreynbd12@gmail.com>
2025-02-12 00:37:44 +0530

    Make a ThemeContext file for dark and lite theme but it is not complete

---
+++ src/context/ThemeContext.jsx
@@ -0,0 +1,27 @@
+import React, { createContext, useEffect, useState } from "react";
+
+export const ThemeContext = createContext();
+
+export const ThemeProvider = ({ children }) => {
+    const storedTheme = localStorage.getItem("theme");
+    const [theme, setTheme] = useState(storedTheme || "light");
+
+    useEffect(() => {
+        if (theme === "dark") {
+            document.documentElement.classList.add("dark");
+        } else {
+            document.documentElement.classList.remove("dark");
+        }
+        localStorage.setItem("theme", theme);
+    }, [theme]);
+
+    const toggleTheme = () => {
+        setTheme((prevTheme) => (prevTheme === "dark" ? "light" : "dark"));
+    };
+
+    return (
+        <ThemeContext.Provider value={{ theme, toggleTheme }}>
+            {children}
+        </ThemeContext.Provider>
+    );
+};
